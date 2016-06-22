c76fe01828eca248f7025258de6a282ca648d4e0
Watschman <Watschmanlp@yahoo.de>
2016-06-22 16:15:22 +0200

    added Song support

--- src/turtleprogramming/Serien/Elf/Aufgabe4.java
+++ src/turtleprogramming/Serien/Elf/Aufgabe4.java
@@ -1,16 +1,21 @@
 package turtleprogramming.Serien.Elf;
 
 import ch.aplu.turtle.Turtle;
+import turtleprogramming.util.SoundHelper;
 
 import java.awt.*;
 import java.awt.geom.Point2D;
+import java.util.Random;
 
 class Aufgabe4 {
 
     final static Color colorTurtle0 = Color.black;
     final static Color colorTurtle1 = Color.red;
+    private static int TurtleScore;
 
     Aufgabe4() {
+        TurtleScore = 0;
+        playMusic();
         Turtle turtle0 = new Turtle();
         Turtle turtle1 = new Turtle(turtle0);
         start(turtle0, colorTurtle0, 100, 0);
@@ -25,10 +30,21 @@ class Aufgabe4 {
             public void run(){
                 while (true) {
                     if (check(turtlea, turtleb, 20)){
-                        turtle.sound(524, 10);
+//                        System.out.println("TurtleScore: " + ">>>>>>" + TurtleScore);
+                        switch (TurtleScore){
+                            case 0:
+                                SoundHelper.playWavFile("FirstBlood");
+                                break;
+                            case 1:
+                                SoundHelper.playWavFile("DoubleKill");
+                                break;
+                            default:
+                                SoundHelper.playWavFile(sound());
+                        }
                         turtle.clean();
                         reset(turtlea, turtlea.getColor(), positionTurtleA);
                         reset(turtleb, turtleb.getColor(), positionTurtleB);
+                        TurtleScore++;
                     }
                     go(turtle);
                 }
@@ -37,16 +53,36 @@ class Aufgabe4 {
     }
     private void go(Turtle turtle)
     {
-        turtle.fd(30 + Math.random()).setRandomHeading();
+        turtle.fd(50 * Math.random()).setRandomHeading();
     }
     private boolean check(Turtle turtlea, Turtle turtleb, double checksize){
         return turtlea.distance(turtleb.getPos()) <= checksize;
+    }
+    private void playMusic(){
+        SoundHelper.playWavFileLoop("music");
     }
     private void start(Turtle turtle, Color color, double x, double y){
         turtle.setColor(color).setPenColor(color).wrap().setPos(x, y);
     }
     private void reset(Turtle turtle, Color color, Point2D.Double Position){
         turtle.setColor(color).setPenColor(color).wrap().setPos(Position);
+    }
+    private String sound(){
+        Random rand = new Random();
+        int number = rand.nextInt(4);
+//        System.out.println("Number: " + ">>>>>>" + number);
+        switch (number){
+            case 0:
+                return "Bullseye";
+            case 1:
+                return "Rampage";
+            case 2:
+                return "BloodBath";
+            case 3:
+                return "Payback";
+            default:
+                return null;
+        }
     }
     public static void main(String[] args){new Aufgabe4();}
 }
---
+++ src/turtleprogramming/util/SoundHelper.java
@@ -0,0 +1,32 @@
+package turtleprogramming.util;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.File;
+
+public class SoundHelper extends Thread{
+    public static void playWavFile(String name){
+        try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("src/turtleprogramming/Song/" + name + ".wav").getAbsoluteFile());
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+        }catch (Exception ex){
+            System.out.println("Exception während playWavFile");
+            ex.printStackTrace();
+        }
+    }
+    public static void playWavFileLoop(String name){
+        try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("src/turtleprogramming/Song/" + name + ".wav").getAbsoluteFile());
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+        }catch (Exception ex){
+            System.out.println("Exception während playWavFile");
+            ex.printStackTrace();
+        }
+    }
+}
